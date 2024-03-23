955564f776d0535a7ffd7d5623f89e2459834517
Ariel Alvarez Gasca <ariel.alvarez.gasca@outlook.com>
2024-03-23 11:33:47 -0600

    feat: user NestJS exceptions objects instead HttpException object and hash user password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { UsersService } from 'src/users/users.service';
+import * as bcryptjs from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -10,23 +16,28 @@ export class AuthService {
   // This service will be used to authenticate user who is logging in. This is only the base code
   async login(user: LoginDto) {
     const { username, password } = user;
+
     // Validate if user exist
     const userFound = await this.usersService.findOneByUsername(username);
+    if (!userFound) throw new NotFoundException('User not found');
 
-    if (!userFound)
-      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    // Validate password match
+    const isPasswordValid = await bcryptjs.compare(
+      password,
+      userFound.password,
+    );
+    if (!isPasswordValid) throw new UnauthorizedException('Incorrect password');
 
-    if (userFound.password === password) return userFound;
-    else throw new HttpException('Incorrect password', HttpStatus.BAD_REQUEST);
+    return userFound;
   }
 
   async register(user: RegisterDto) {
     // Validate if user already exist
     const { username } = user;
+    user.password = await bcryptjs.hash(user.password, 10);
     const userFound = await this.usersService.findOneByUsername(username);
 
-    if (userFound)
-      throw new HttpException('User already exists', HttpStatus.CONFLICT);
+    if (userFound) throw new BadRequestException('User already exists');
 
     return this.usersService.create(user);
   }
