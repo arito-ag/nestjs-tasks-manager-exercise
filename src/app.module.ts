fe912919e4c8bac1560c78228af79fc6e31fe997
Ariel Alvarez Gasca <ariel.alvarez.gasca@outlook.com>
2024-03-22 19:01:48 -0600

    feat: Add typeorm and mysql packages and set in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Init config to TypeORM to connect with database, load entities and synchronize Entities from project with Tables in db
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: '',
+      password: '',
+      database: '',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
