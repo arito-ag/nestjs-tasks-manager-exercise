4f0e62159d94c744ff56783a5e1723bcadd56abe
Ariel Alvarez Gasca <ariel.alvarez.gasca@outlook.com>
2024-03-24 11:03:58 -0600

    feat: Relation between User and Transactions and remove unused Transaction Controller

---
+++ src/transactions/entitites/transaction.entity.ts
@@ -0,0 +1,23 @@
+import { User } from 'src/users/entities/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'transactions' })
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  type: string;
+
+  @Column({ nullable: false })
+  description: string;
+
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ nullable: false })
+  userId: number;
+
+  @ManyToOne(() => User, (user) => user.transactions)
+  user: User;
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Task } from 'src/tasks/entities/task.entity';
+import { Transaction } from 'src/transactions/entitites/transaction.entity';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'users' })
@@ -17,4 +18,7 @@ export class User {
 
   @OneToMany(() => Task, (task) => task.user)
   tasks: Task[];
+
+  @OneToMany(() => Transaction, (transaction) => transaction.user)
+  transactions: Transaction[];
 }
