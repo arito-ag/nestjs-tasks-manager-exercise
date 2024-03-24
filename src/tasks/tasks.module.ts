d90bc58251cfcc8a7eefc37c749f873def925663
Ariel Alvarez Gasca <ariel.alvarez.gasca@outlook.com>
2024-03-24 12:01:52 -0600

    feat: Implements Transactions log in Create, Update and Delete task by User

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,9 +4,10 @@ import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
 import { UsersModule } from 'src/users/users.module';
+import { TransactionsModule } from 'src/transactions/transactions.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Task]), UsersModule, TransactionsModule],
   controllers: [TasksController],
   providers: [TasksService],
   exports: [TasksService],
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,6 @@
 import {
   BadRequestException,
   Injectable,
-  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -11,16 +10,19 @@ import { Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TASK_STATUS } from './constants/status';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { TransactionsService } from 'src/transactions/transactions.service';
+import { TRANSACTION_TYPES } from 'src/transactions/constants/types';
 
 @Injectable()
 export class TasksService {
   constructor(
     @InjectRepository(Task) private readonly taskRepository: Repository<Task>,
-    private readonly userService: UsersService,
+    private readonly usersService: UsersService,
+    private readonly transactionsService: TransactionsService,
   ) {}
 
   async getAll(username: string) {
-    const userFound = await this.userService.findByUsername(username);
+    const userFound = await this.usersService.findByUsername(username);
     const userId = userFound.id;
     return this.taskRepository.find({ where: { userId } });
   }
@@ -33,26 +35,37 @@ export class TasksService {
     });
 
     if (!taskFound) throw new UnauthorizedException();
+
     return taskFound;
   }
 
   async getByFilters(username: string) {
-    const userFound = await this.userService.findByUsername(username);
+    const userFound = await this.usersService.findByUsername(username);
     const userId = userFound.id;
     return this.taskRepository.find({ where: { userId } });
   }
 
   async create(username: string, task: CreateTaskDto) {
-    const userFound = await this.userService.findByUsername(username);
-    if (!userFound) throw new NotFoundException('User not found');
+    const userFound = await this.usersService.findByUsername(username);
 
     task['status'] = TASK_STATUS.PENDING;
     const newTask = this.taskRepository.create(task);
     newTask.userId = userFound.id;
-    return this.taskRepository.save(newTask);
+
+    const taskCreated = await this.taskRepository.save(newTask);
+
+    await this.transactionsService.create(
+      TRANSACTION_TYPES.TASK.COMPLETED.CREATED,
+      `Task ${newTask.id} created by ${username}`,
+      userFound.id,
+    );
+
+    return taskCreated;
   }
 
   async update(id: number, task: UpdateTaskDto) {
+    const { task: taskInfo, user } = await this.getTaskAndUserInfo(id);
+
     const validateStatus =
       task.status !== undefined ? this.findStatusInConstant(task.status) : true;
 
@@ -61,11 +74,27 @@ export class TasksService {
         `That Task Status: "${task.status}" is not allowed`,
       );
 
-    return this.taskRepository.update(id, task);
+    const taskUpdated = await this.taskRepository.update(id, task);
+
+    await this.transactionsService.create(
+      TRANSACTION_TYPES.TASK.COMPLETED.UPDATED,
+      `Task ${id} updated by ${user.username}`,
+      taskInfo.userId,
+    );
+
+    return taskUpdated;
   }
 
   async delete(id: number) {
+    const { task, user } = await this.getTaskAndUserInfo(id);
     const deletedTask = await this.taskRepository.delete({ id });
+
+    await this.transactionsService.create(
+      TRANSACTION_TYPES.TASK.COMPLETED.DELETED,
+      `Task ${id} deleted by ${user.username}`,
+      task.userId,
+    );
+
     return deletedTask;
   }
 
@@ -80,5 +109,15 @@ export class TasksService {
   private findStatusInConstant(status: string): boolean {
     const keys = Object.values(TASK_STATUS);
     return keys.includes(status);
+  }
+
+  private async getTaskAndUserInfo(taskId: number) {
+    const task = await this.taskRepository.findOne({ where: { id: taskId } });
+    const user = await this.usersService.findById(task.userId);
+
+    return {
+      task,
+      user,
+    };
   }
 }
