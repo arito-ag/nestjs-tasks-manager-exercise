cb99caac8a7973d7b593c4bfd17e4a6cd2e8ca62
Ariel Alvarez Gasca <ariel.alvarez.gasca@outlook.com>
2024-03-25 14:37:00 -0600

    feat: implements ThrottlerModule to prevent brute force attacks

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 4000,
+        limit: 3,
+      },
+    ]),
+  ],
   controllers: [UploadController],
-  providers: [UploadService],
+  providers: [
+    UploadService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   exports: [UploadService],
 })
 export class UploadModule {}
